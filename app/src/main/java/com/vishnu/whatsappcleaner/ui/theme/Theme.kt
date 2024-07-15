fbe6390bf973352c31574c9fbd26f9ddd8c653af
VishnuSanal <t.v.s10123@gmail.com>
2024-07-15 12:25:19 +0530

    change font

--- app/src/main/java/com/vishnu/whatsappcleaner/ui/theme/Theme.kt
+++ app/src/main/java/com/vishnu/whatsappcleaner/ui/theme/Theme.kt
@@ -3,12 +3,16 @@ package com.vishnu.whatsappcleaner.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.vishnu.whatsappcleaner.R
 
 private val DarkColorScheme = darkColorScheme(
     primary = primaryColor,
@@ -21,6 +25,8 @@ private val LightColorScheme = lightColorScheme(
     secondary = secondaryColor,
     tertiary = tertiaryColor
 )
+
+private val fontFamily = FontFamily(Font(R.font.geist))
 
 @Composable
 fun WhatsAppCleanerTheme(
@@ -38,6 +44,28 @@ fun WhatsAppCleanerTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
+    val Typography = Typography(
+        displayLarge = Typography.displayLarge.copy(fontFamily = fontFamily),
+        displayMedium = Typography.displayMedium.copy(fontFamily = fontFamily),
+        displaySmall = Typography.displaySmall.copy(fontFamily = fontFamily),
+
+        headlineLarge = Typography.headlineLarge.copy(fontFamily = fontFamily),
+        headlineMedium = Typography.headlineMedium.copy(fontFamily = fontFamily),
+        headlineSmall = Typography.headlineSmall.copy(fontFamily = fontFamily),
+
+        titleLarge = Typography.titleLarge.copy(fontFamily = fontFamily),
+        titleMedium = Typography.titleMedium.copy(fontFamily = fontFamily),
+        titleSmall = Typography.titleSmall.copy(fontFamily = fontFamily),
+
+        bodyLarge = Typography.bodyLarge.copy(fontFamily = fontFamily),
+        bodyMedium = Typography.bodyMedium.copy(fontFamily = fontFamily),
+        bodySmall = Typography.bodySmall.copy(fontFamily = fontFamily),
+
+        labelLarge = Typography.labelLarge.copy(fontFamily = fontFamily),
+        labelMedium = Typography.labelMedium.copy(fontFamily = fontFamily),
+        labelSmall = Typography.labelSmall.copy(fontFamily = fontFamily)
+    )
 
     MaterialTheme(
         colorScheme = colorScheme,
