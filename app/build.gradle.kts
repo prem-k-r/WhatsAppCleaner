a2580af98ce7acb891d5ea23b8ac1de50a7f6d02
Vishnu Sanal T <t.v.s10123@gmail.com>
2024-10-27 20:53:36 +0530

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "v1.0.0-alpha"
+        versionName = "v0.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
