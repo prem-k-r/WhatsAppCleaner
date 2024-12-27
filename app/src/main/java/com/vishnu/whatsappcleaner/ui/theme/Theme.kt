9c70155fa07038584d36f41e954b29ce729ebe51
Vishnu Sanal T <t.v.s10123@gmail.com>
2024-12-27 19:23:03 +0530

    disable dynamic theming

--- app/src/main/java/com/vishnu/whatsappcleaner/ui/theme/Theme.kt
+++ app/src/main/java/com/vishnu/whatsappcleaner/ui/theme/Theme.kt
@@ -101,7 +101,7 @@ else
 fun WhatsAppCleanerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
