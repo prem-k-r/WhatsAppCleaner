f47fad1d046bd03c52e408b990cfe420cbf44cca
Vishnu Sanal T <t.v.s10123@gmail.com>
2025-02-28 23:49:57 +0530

    v1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vishnu.whatsappcleaner"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "v1.0.2"
+        versionCode = 6
+        versionName = "v1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
